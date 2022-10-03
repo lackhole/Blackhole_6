52a0e9b70602875d87a50c3a6cff7dcebd0ef9ed
Tony <cosgenio@gmail.com>
2022-10-03 20:13:03 +0900

    Use C++17 filesystem

---
+++ config.h
@@ -0,0 +1,15 @@
+//
+// Created by YongGyu Lee on 2022/10/02.
+//
+
+#ifndef BLACKHOLE_6_CONFIG_H_
+#define BLACKHOLE_6_CONFIG_H_
+
+#if __cplusplus > 201406L
+  #define register
+#else
+  #define register register
+#endif
+
+
+#endif // BLACKHOLE_6_CONFIG_H_
--- matrix.h
+++ matrix.h
@@ -2,7 +2,6 @@
 
 #include <math.h>
 #include <iostream>
-#include <Windows.h>
 
 namespace BH_MATRIX {
 	typedef double e_type;
